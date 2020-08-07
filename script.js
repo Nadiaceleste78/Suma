5be72dae4bd74b9d2fce72e1162c8792d14ceb6b
Nadia <ferreyranadia78@gmail.com>
2020-08-07 11:32:41 -0300

    Primer ejercicio (suma) del trabajo práctico terminado

---
+++ script.js
@@ -0,0 +1,32 @@
+/*let numeros = [1,2,3];
+
+let suma=0;
+
+function sumarElementos (numeros) {
+  
+  if (suma = numero + suma) {
+
+    mensaje.innerHTML = `<li> La suma de los números es : ${suma} .</li>`
+
+  }git add
+}
+  for (numero of numeros){
+
+    sumarElementos (numero)
+
+  }*/
+
+
+
+  function sumarElementos(numeros) {
+    let suma = 0;
+  
+    // Generar la lógica acá
+
+    return suma;
+}
+
+let numeros = [1, 2, 3];
+
+// Ejecutamos la función e imprimimos el resultado en la consola.
+console.log(sumarElementos(numeros));
